16d9c063f2f85b78113f43d7cb4c016075974913
apolloswagmoney <93349308+apolloswagmoney@users.noreply.github.com>
2026-03-18 21:16:37 -0230

    Skip consecutive duplicate points in path computation

--- src/util/computePath.ts
+++ src/util/computePath.ts
@@ -7,11 +7,15 @@ export function computePath(points: Point[], borderRadius?: number): string {
   // Starting position
   d += `M ${points[0].x} ${points[0].y} `
   
-  // Draw lines between all points in a cycle
+  // Draw lines between all points in the array, creating a cycle
   for (let i = 0; i < l; i++) {
-    d += `L ${points[(i + 1) % l].x} ${points[(i + 1) % l].y} `
+    let curr = points[i];
+    let next = points[(i + 1) % l];
+    // Skip repeated points if they appear
+    if ((curr.x !== next.x) || (curr.y !== next.y)){
+      d += `L ${next.x} ${next.y} `
+    }
   }
 
-  d += `Z`;
-  return d;
+  return d += 'Z';
 }
